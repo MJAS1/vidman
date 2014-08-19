052a2923901c31c0b57020d609ce6d3e6caf4d65
Manu Sutela <manu.sutela@aalto.fi>
2014-08-19 14:25:48 +0300

    Now working with 60 FPS at least in ubuntu 14.04 (for some reason only 30 FPS works with ubuntu 12.04). Qt had to be downgraded to version 4.8, because vsync wasn't working with current Qt 5. Retry when new version is released. GLThread class added to handle frame buffer swapping.

---
+++ glthread.cpp
@@ -0,0 +1,5 @@
+#include "glthread.h"
+
+GLThread::GLThread()
+{
+}
---
+++ glthread.h
@@ -0,0 +1,10 @@
+#ifndef GLTHREAD_H
+#define GLTHREAD_H
+
+class GLThread : public StoppableThread
+{
+public:
+    GLThread();
+};
+
+#endif // GLTHREAD_H
