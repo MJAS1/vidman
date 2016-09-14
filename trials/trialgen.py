2630cb5b1e6efa64ee20ebd4dfe400cff526c044
Sutela Manu <sutelam1@anius.org.aalto.fi>
2016-09-14 16:34:14 +0300

    Updated the trial for pilot run. No more fadeouts and fadeins

--- trials/trialgen.py
+++ trials/trialgen.py
@@ -11,65 +11,50 @@ file = open(filename, 'w')
 file.truncate();
 
 imgFilename = "../img/button.png"
-fadeDuration = 2000
-fadeoutTrigCode = 1
-fadeinTrigCode = 2
-imgTrigCode = 3
-standardTrigCode = 4
-deviantTrigCode = 5
-blackDuration = 1000
+imgTrigCode = 1
+standardTrigCode = 2
+deviantTrigCode = 3
 minDelay = 1000
 maxDelay = 2000
-imgDuration = 1000
-imgDelay = 1000
+imgDuration = 700
 x = 400
 y = 100
 
 file.write("Object: type=image, id=0, filename="+imgFilename+"\n\n")
+
 trials = []
+trials = ["standard"]*360
+trials.extend(["deviant"]*40)
+random.shuffle(trials)
+#Start each trial with only standards
+trials[0:0] = 20*["standard"]
 
-trials = ["normal"]*5
-
-for i in range(0, 79):
-    ls = ["normal"]*4
-    ls.append("deviant")
-    random.shuffle(ls)
-    trials.extend(ls)
-
-for i in range(len(trials)-1):
-    if trials[i] == "deviant" and trials[i+1] == "deviant":
-        trials[i], trials[i-1] = trials[i-1], trials[i]
+#Make sure a fixed number of standard runs follow each deviant
+i = 0
+while i < len(trials):
+    if trials[i] == "deviant": 
+        trials[i+1:i+1] = 5*["standard"]
+        i = i + 6
+    else:
+        i = i + 1
 
 for trial in trials:
-    if trial == "normal":
-        file.write("Event: type=fadeout, start=0, duration={}, delay={}," \
-                " trigcode={}\n".format(fadeDuration, fadeDuration, fadeoutTrigCode))
-        file.write("Delete: start=0, type=freeze\n")
-        file.write("Event: type=fadein, start={}, duration={}, delay={}," \
-                 "trigcode={}\n".format(blackDuration, fadeDuration, 
-                                        fadeDuration, fadeinTrigCode))
-        file.write("Event: type=detectmotion, trigCode={}\n".format(standardTrigCode))
-
+    if trial == "standard":
         delay = random.randint(minDelay, maxDelay)
-        file.write("Event: type=image, start={}, x={}, y={}, objectid=0," \
-                 "delay={}, trigcode={}\n".format(delay, x, y, imgDuration, imgTrigCode))
-        file.write("Delete: start={}, type=image, delay={}\n".format(imgDuration, imgDelay))
-        file.write("\n")
+        file.write("Event: type=detectmotion, trigCode={}, start={}\n".format(standardTrigCode, delay))
+        file.write("Event: type=image, x={}, y={}, objectid=0," \
+                 "delay={}, trigcode={}\n".format(x, y, imgDuration, imgTrigCode))
+        file.write("Delete: start={}, type=image\n".format(imgDuration))
+        file.write("Delete: start=0, type=detectmotion\n")
 
     else:
-        file.write("Event: type=fadeout, start=0, duration={}, delay={}," \
-                " trigcode={}\n".format(fadeDuration, fadeDuration, fadeoutTrigCode))
-        file.write("Delete: start=0, type=freeze\n")
-        file.write("Event: type=freeze, start=0\n")
-        file.write("Event: type=fadein, start={}, duration={}, delay={}," \
-                 "trigcode={}\n".format(blackDuration, fadeDuration, 
-                                        fadeDuration, fadeinTrigCode))
-        file.write("Event: type=detectmotion, trigCode={}\n".format(deviantTrigCode))
-
         delay = random.randint(minDelay, maxDelay)
-        file.write("Event: type=image, start={}, x={}, y={}, objectid=0," \
-                 "delay={}, trigcode={}\n".format(delay, x, y, imgDuration, imgTrigCode))
-        file.write("Delete: start={}, type=image, delay={}\n".format(imgDuration, imgDelay))
-        file.write("\n")
+        file.write("Event: type=detectmotion, trigCode={}, start={}\n".format(deviantTrigCode, delay))
+        file.write("Event: type=freeze, start=0\n")
+        file.write("Event: type=image, x={}, y={}, objectid=0," \
+                 "delay={}, trigcode={}\n".format(x, y, imgDuration, imgTrigCode))
+        file.write("Delete: start={}, type=image\n".format(imgDuration))
+        file.write("Delete: start=0, type=freeze\n")
+        file.write("Delete: start=0, type=detectmotion\n")
 
 file.close();
